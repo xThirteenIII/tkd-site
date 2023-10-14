7fa2d002f24520fc36bafc55ec2a94f39da7c514
th1rt3en <giuseppe.richetti@gmail.com>
2023-10-14 14:59:08 +0200

    Add html, css and js files

---
+++ javascript/homepage.js
@@ -0,0 +1,28 @@
+let menu = document.querySelector("#menu-btn");
+let navbar = document.querySelector(".header .navbar");
+
+menu.onclick = () =>{
+    menu.classList.toggle("fa-times");
+    navbar.classList.toggle("active");
+}
+
+window.onscroll = () =>{
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+};
+
+var swiper = new Swiper(".home-slider", {
+    spaceBetween: 20,
+    effect: "fade",
+    grabCursor: true,
+    loop:true,
+    centeredSlides: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    autoplay:{
+        delay:3000,
+        disableOnInteraction: false,
+    },
+});
